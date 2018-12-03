c6650ea018368f14a23db6def79911b264a8686b
Mirage-A <zaitsev.eo@phystech.edu>
2018-12-03 14:55:17 +0300

    added setScreenSize() method and updated tests setScreenSize uses calculateViewportSize method to calculate an optimal virtual screen size and update it

--- core/src/com/mirage/view/View.java
+++ core/src/com/mirage/view/View.java
@@ -8,25 +8,43 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.mirage.model.ModelFacade;
+import com.mirage.model.scene.Scene;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class View {
-    // Эталонный размер экрана
-    // Все изображения рисуются под этот размер
-    // Для других экранов размеры изображений подгоняются так,
-    // чтобы различие с эталонным размером экрана было минимально,
-    // но при этом чтобы изображения не сплющивались и не растягивались,
-    // т.е. отношение ширины и высоты всех изображений сохранялось.
-    protected final int DEFAULT_SCREEN_WIDTH = 1920;
-    protected final int DEFAULT_SCREEN_HEIGHT = 1080;
-    protected final int DEFAULT_TILE_WIDTH = 128;
-    protected final int DEFAULT_TILE_HEIGHT = 64;
+    /**
+     * Эталонный размер экрана
+     * Все изображения рисуются под этот размер
+     * Для других экранов размеры изображений подгоняются так,
+     * чтобы различие с эталонным размером экрана было минимально,
+     * но при этом чтобы изображения не сплющивались и не растягивались,
+     * т.е. отношение ширины и высоты всех изображений сохранялось.
+     */
+    private final int DEFAULT_SCREEN_WIDTH = 1920;
+    private final int DEFAULT_SCREEN_HEIGHT = 1080;
+    /**
+     * Размер одного тайла на виртуальном экране
+     */
+    private final int TILE_WIDTH = 128;
+    private final int TILE_HEIGHT = 64;
+
+    /**
+     * Список текстур, используемых на данной сцене (карте)
+     */
+    protected List<Texture> tileTextures;
 
     private Texture dropImage;
     private Texture bucketImage;
     private SpriteBatch batch;
     public OrthographicCamera camera;
     private ModelFacade model;
-    protected int scrW, scrH; // Размер экрана
+    /**
+     * Размеры виртуального экрана
+     */
+    protected int scrW;
+    protected int scrH;
 
 
     public View(ModelFacade model) {
@@ -38,7 +56,6 @@ public class View {
 
         // create the camera and the SpriteBatch
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, 1920, 1080);
         batch = new SpriteBatch();
 
     }
@@ -76,23 +93,54 @@ public class View {
         batch.dispose();
     }
 
+    /**
+     * Метод, который должен вызываться при изменении параметров реального экрана.
+     * @param realWidth Ширина реального экрана
+     * @param realHeight Высота реального экрана
+     */
+    public void setScreenSize(int realWidth, int realHeight) {
+        Rectangle viewportSize = calculateViewportSize(realWidth, realHeight);
+        scrW = (int) viewportSize.width;
+        scrH = (int) viewportSize.height;
+        camera.setToOrtho(false, scrW, scrH);
+    }
+
+    /**
+     * Вычисляет оптимальный размер виртуального экрана так,
+     * чтобы размер тайлов после скейла под экран был равен вычисленному
+     * с помощью calculateTileSize,
+     * при этом гарантируется, что и ширина, и высота виртуального экрана обязательно четные.
+     * @param realWidth Ширина реального экрана
+     * @param realHeight Высота реального экрана
+     * @return Оптимальный размер виртуального экрана
+     */
+    protected Rectangle calculateViewportSize(int realWidth, int realHeight) {
+        Rectangle tileSize = calculateTileSize(realWidth, realHeight);
+        int width = evenRound(realWidth / tileSize.width * TILE_WIDTH);
+        int height = evenRound(realHeight / tileSize.height * TILE_HEIGHT);
+        Log.i("Размеры виртуального экрана: " + width + " x " + height + " px");
+        return new Rectangle(0, 0, width, height);
+    }
 
     /**
      * Вычисляет размер одного тайла на экране в зависимости от параметров экрана.
      * Размеры тайла подбираются таким образом, чтобы площадь (в тайлах)
      * симметрической разности эталонного и полученного обзоров была минимальна.
-     * Учитывается, что отношение ширины и высоты тайла равно 2.
+     * Учитывается, что отношение ширины и высоты тайла равно 2,
+     * при этом гарантируется, что и ширина, и высота обязательно четные.
+     * @param realWidth Ширина реального экрана
+     * @param realHeight Высота реального экрана
      * @return Оптимальный размер одного тайла (хранится как ширина и высота прямоугольника)
      */
-    protected Rectangle calculateTileSize() {
+    protected Rectangle calculateTileSize(int realWidth, int realHeight) {
         // Используем тернарный поиск по высоте тайла
         float L = 0f;
         float R = 512f;
         while (R - L > 0.01f) {
             float mid1 = L + (R - L) / 3;
             float mid2 = L + (R - L) * 2 / 3;
-            float diff1 = visionsSymmetricDifference(mid1);
-            float diff2 = visionsSymmetricDifference(mid2);
+            float diff1 = visionsSymmetricDifference(mid1, realWidth, realHeight);
+            float diff2 = visionsSymmetricDifference(mid2, realWidth, realHeight);
             if (diff1 < diff2) {
                 R = mid2;
             }
@@ -100,12 +148,12 @@ public class View {
                 L = mid1;
             }
         }
-        float tileH = Math.round(L);
+        int tileH = evenRound(L);
         Rectangle res = new Rectangle(0, 0, 2 * tileH, tileH);
 
-        Log.i("Размеры экрана: " + scrW + " x " + scrH + " px");
+        Log.i("Размеры экрана: " + realWidth + " x " + realHeight + " px");
         Log.i("Размеры тайла: " + res.width + " x " + res.height + " px");
-        Log.i("Кол-во тайлов на экране: " + scrW/res.width + " x " + scrH/res.height);
+        Log.i("Кол-во тайлов на экране: " + realWidth/res.width + " x " + realHeight/res.height);
 
         return res;
     }
@@ -115,13 +163,15 @@ public class View {
      * По данной высоте тайла вычисляет площадь симметрической разности эталонного и
      * возможного при данной высоте обзора.
      * @param tileH Высота тайла
+     * @param realWidth Ширина реального экрана
+     * @param realHeight Высота реального экрана
      * @return Площадь симметрической разности обзоров
      */
-    private float visionsSymmetricDifference(float tileH) {
-        float w = scrW / (2 * tileH);
-        float h = scrH / tileH;
-        float dw = DEFAULT_SCREEN_WIDTH / (float) DEFAULT_TILE_WIDTH;
-        float dh = DEFAULT_SCREEN_HEIGHT / (float) DEFAULT_TILE_HEIGHT;
+    private float visionsSymmetricDifference(float tileH, int realWidth, int realHeight) {
+        float w = realWidth / (2 * tileH);
+        float h = realHeight / tileH;
+        float dw = DEFAULT_SCREEN_WIDTH / (float) TILE_WIDTH;
+        float dh = DEFAULT_SCREEN_HEIGHT / (float) TILE_HEIGHT;
 
         if (w <= dw && h <= dh) {
             return dw * dh - w * h;
@@ -133,5 +183,35 @@ public class View {
                 Math.abs(h - dh) * Math.min(w, dw);
     }
 
+    /**
+     * Вспомогательная функция, необходимая в calculateTileSize.
+     * Округляет число a до ближайшего четного целого числа.
+     * Если a - целое нечетное, то округление вверх.
+     * @param a Число
+     * @return Ближайшее четное целое число
+     */
+    private int evenRound(float a) {
+        int res = Math.round(a);
+        if (res % 2 == 0) {
+            return res;
+        }
+        if (a < res) {
+            return res - 1;
+        }
+        return res + 1;
+    }
+
+    /**
+     * Загружает текстуры тайлов, используемых в данной сцене
+     * //TODO
+     */
+    protected void loadTileTextures(Scene scene) {
+        tileTextures = new ArrayList<Texture>();
+        tileTextures.add(new Texture(Gdx.files.internal("android/assets/tiles/0000.png")));
+    }
+
+    /**
+     * Пустой protected конструктор, нужен только для тестов
+     */
     protected View() {}
 }
--- core/tests/ViewTest.java
+++ core/tests/ViewTest.java
@@ -1,3 +1,5 @@
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Rectangle;
 import com.mirage.view.View;
 
@@ -9,17 +11,17 @@ public class ViewTest extends View {
     @Test
     public void test() {
         System.out.println("Test!");
-        scrW = 1920;
-        scrH = 1080;
-        assertEquals(new Rectangle(0, 0, 128, 64), calculateTileSize());
-        scrW = 960;
-        scrH = 540;
-        assertEquals(new Rectangle(0, 0, 64, 32), calculateTileSize());
-        scrW = 1600;
-        scrH = 900;
-        assertEquals(new Rectangle(0, 0, 106, 53), calculateTileSize());
-        scrW = 480;
-        scrH = 320;
-        assertEquals(new Rectangle(0, 0, 38, 19), calculateTileSize());
+
+        assertEquals(new Rectangle(0, 0, 128, 64), calculateTileSize(1920, 1080));
+        assertEquals(new Rectangle(0, 0, 1920, 1080), calculateViewportSize(1920, 1080));
+
+        assertEquals(new Rectangle(0, 0, 64, 32), calculateTileSize(960, 540));
+        assertEquals(new Rectangle(0, 0, 1920, 1080), calculateViewportSize(1920, 1080));
+
+        assertEquals(new Rectangle(0, 0, 108, 54), calculateTileSize(1600, 900));
+        assertEquals(new Rectangle(0, 0, 1896, 1066), calculateViewportSize(1600, 900));
+
+        assertEquals(new Rectangle(0, 0, 36, 18), calculateTileSize(480, 320));
+        assertEquals(new Rectangle(0, 0, 1706, 1138), calculateViewportSize(480, 320));
     }
 }
