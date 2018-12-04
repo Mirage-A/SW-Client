b4b0c3e1cff60ac6b39462cfccd6bd7b25d60f45
Mirage-A <zaitsev.eo@phystech.edu>
2018-12-04 23:55:50 +0300

    started work with generated animation class

--- core/src/com/mirage/view/scene/objects/humanoid/HumanoidDrawer.kt
+++ core/src/com/mirage/view/scene/objects/humanoid/HumanoidDrawer.kt
@@ -59,7 +59,7 @@ class HumanoidDrawer : AnimatedObjectDrawer {
 
 
     fun curValue(startValue: Float, endValue : Float, progress : Float) : Float {
-        return startValue + (endValue - startValue) * progress / 1000f
+        return startValue + (endValue - startValue) * progress
     }
     override fun draw(batch: SpriteBatch, x: Float, y: Float, timePassedSinceStart: Long) {
         var timePassed = 0f + timePassedSinceStart % 1000
@@ -70,9 +70,15 @@ class HumanoidDrawer : AnimatedObjectDrawer {
                         when (weaponType) {
                             WeaponType.ONE_HANDED ->
                             {
-                                if (timePassed < 1001)
+                                if (timePassed < 501)
                                 {
-                                    batch.draw(textures["cloak"]!!.getTexture(timePassedSinceStart),x + curValue(1.0f, 1.0f, timePassed),y + curValue(3.0f, 3.0f, timePassed),curValue(120.0f, 55.0f, timePassed)/2,curValue(109.0f, 50.0f,timePassed)/2,curValue(120.0f, 55.0f, timePassed),curValue(109.0f, 50.0f, timePassed),1f, 1f,curValue(89.67626f, 89.67626f, timePassed),0, 0,DefaultSizes.defaultWidth["cloak"]!!, DefaultSizes.defaultHeight["cloak"]!!, false, false)
+                                    val progress = (timePassed - 0) / 501f
+                                    batch.draw(textures["cloak"]!!.getTexture(timePassedSinceStart), x + curValue(1.0f, 1.0f, progress) - DefaultSizes.defaultWidth["cloak"]!!/2f, y + curValue(3.0f, 3.0f, progress) - DefaultSizes.defaultHeight["cloak"]!!/2f, DefaultSizes.defaultWidth["cloak"]!!/2f, DefaultSizes.defaultHeight["cloak"]!!/2f, DefaultSizes.defaultWidth["cloak"]!! + 0f, DefaultSizes.defaultHeight["cloak"]!! + 0f, curValue(120.0f, 54.0f, progress) / (DefaultSizes.defaultWidth["cloak"]!! + 0f), curValue(109.0f, 49.0f, progress) / (DefaultSizes.defaultHeight["cloak"]!! + 0f), curValue(-0.3237381f, 89.526474f, progress), 0, 0, DefaultSizes.defaultWidth["cloak"]!!, DefaultSizes.defaultHeight["cloak"]!!, false, false)
+                                }
+                                else if (timePassed < 1002)
+                                {
+                                    val progress = (timePassed - 501) / 501f
+                                    batch.draw(textures["cloak"]!!.getTexture(timePassedSinceStart), x + curValue(1.0f, 1.0f, progress) - DefaultSizes.defaultWidth["cloak"]!!/2f, y + curValue(3.0f, 3.0f, progress) - DefaultSizes.defaultHeight["cloak"]!!/2f, DefaultSizes.defaultWidth["cloak"]!!/2f, DefaultSizes.defaultHeight["cloak"]!!/2f, DefaultSizes.defaultWidth["cloak"]!! + 0f, DefaultSizes.defaultHeight["cloak"]!! + 0f, curValue(54.0f, 120.0f, progress) / (DefaultSizes.defaultWidth["cloak"]!! + 0f), curValue(49.0f, 109.0f, progress) / (DefaultSizes.defaultHeight["cloak"]!! + 0f), curValue(89.526474f, -0.3237381f, progress), 0, 0, DefaultSizes.defaultWidth["cloak"]!!, DefaultSizes.defaultHeight["cloak"]!!, false, false)
                                 }
                             }
                             WeaponType.ONE_HANDED_AND_SHIELD ->
