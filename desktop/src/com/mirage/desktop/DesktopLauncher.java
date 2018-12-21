1459aa0a2665320c54fc7999b0bfc7f7468b6843
Mirage-A <zaitsev.eo@phystech.edu>
2018-12-22 01:41:36 +0300

    configured shadow plugin for gradle, created jar file

--- desktop/src/com/mirage/desktop/DesktopLauncher.java
+++ desktop/src/com/mirage/desktop/DesktopLauncher.java
@@ -9,15 +9,15 @@ import com.mirage.view.TextureLoader;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		System.setProperty("user.name","CorrectUserName");
+		//TODO ВАЖНО! При создании jar-архива эта строка должна быть пустой!
 		TextureLoader.ASSETS_PATH = "./android/assets/";
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Shattered World";
-		config.addIcon("./android/res/drawable/windows_icon.png", Files.FileType.Internal);
-		config.addIcon("./android/res/drawable/mac_icon.png", Files.FileType.Internal);
-		config.addIcon("./android/res/drawable/ic_launcher.png", Files.FileType.Internal);
+		config.addIcon(TextureLoader.ASSETS_PATH + "windows_icon.png", Files.FileType.Internal);
+		config.addIcon(TextureLoader.ASSETS_PATH + "mac_icon.png", Files.FileType.Internal);
 		// Фуллскрин
 		//config.fullscreen = true;
-		//config.width = 111; // Костыль, без которого фулскрин ломается
+		//config.width = 111; // Костыль, без которого фулскрин ломается :(
 		new LwjglApplication(new Controller(), config);
 	}
 }
