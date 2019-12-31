6f7386e0b43b65c205443e2193bd47a26c78ceb2
Mirage-A <zaitsev.eo@phystech.edu>
2019-12-31 18:48:02 +0300

    minor refactoring

--- core/src/main/kotlin/com/mirage/core/extensions/MathExtensions.kt
+++ core/src/main/kotlin/com/mirage/core/extensions/MathExtensions.kt
@@ -6,6 +6,7 @@ import kotlin.math.roundToInt
 fun sqr(a: Float) = a * a
 
 fun Float.trunc() : Int {
+    val b = 0
     val a = this.roundToInt()
     return if (this >= a.toFloat()) a
     else a - 1
